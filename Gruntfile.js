339b87d057d8128bb998648c980e3c506882edcb
Joshua Thornton <jshthornton@gmail.com>
2013-07-08 22:00:38 +0100

    dists. Gruntfile to uglify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,31 @@
 module.exports = function(grunt) {
-	
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				// the banner is inserted at the top of the output
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+				mangle: true,
+				compress: true,
+				preserveComments: false
+			},
+			dist: {
+				files: {
+					//Singles
+					'dist/<%= pkg.name %>.min.js': ['src/lid.js'],
+					'dist/extensions/parse.min.js': ['src/extensions/parse.js'],
+
+					//Bundled
+					'dist/bundles/parse/<%= pkg.name %>.min.js': ['src/lid.js', 'src/extensions/parse.js']
+				}
+			}
+		}
+	});
+
+	//Loads
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	//Tasks
+	grunt.registerTask('default', ['uglify']);
 };
